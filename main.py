49b91ef7d5b56b44a25c4a74f324ef72334f3c1b
Darrin <darrinbuenger@gmail.com>
2022-11-09 22:01:24 -0800

    generic for more computers

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.common.by import By
 #functions file import
 import linkedin_functions
-
+import socket
 #import pandas
 import pandas as pd
 import time
@@ -26,9 +26,10 @@ prefs = {"credentials_enable_service": False,
 options.add_experimental_option("prefs", prefs)
 #options.add_experimental_option("credentials_enable_service", False)
 #options.add_experimental_option("profile.password_manager_enabled", False)
-service = ChromeService(executable_path=r"C:\Users\dbuenger\source\repos\WebScrape\chromedriver.exe")
-#service = ChromeService(executable_path=r"C:\Users\darri\PycharmProjects\WebScrape\chromedriver.exe")
-
+if socket.gethostname() == "DARRIN-LAPTOP":
+    service = ChromeService(executable_path=r"C:\Users\darri\source\repos\WebScrape\chromedriver.exe")
+else:    
+    service = ChromeService(executable_path=r"C:\Users\dbuenger\source\repos\WebScrape\chromedriver.exe")
 
 driver = webdriver.Chrome(service=service, options=options)
 
